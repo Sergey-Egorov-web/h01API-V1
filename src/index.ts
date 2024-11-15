f693943903ae7c3f9ba569b1218ad00861e67430
Sergey-Egorov-web <ispanezzz.foto@gmail.com>
2024-11-15 22:30:06 +0300

    added delete and put request

--- src/index.ts
+++ src/index.ts
@@ -82,6 +82,72 @@ app.post("/videos", (req: Request, res: Response) => {
   res.status(201).send(newVideo);
 });
 
+app.put("/videos/:id", (req: Request, res: Response) => {
+  let video = videos.find((p) => p.id === +req.params.id);
+
+  if (!video) {
+    res.send(404);
+  } else {
+    // const errorsMessages: { message: string; field: string }[] = [];
+
+    // if (
+    //   // !req.body.title ||
+    //   // typeof req.body.title !== "string" ||
+    //   req.body.title.length > 40
+    // ) {
+    //   errorsMessages.push({
+    //     message: "Invalid title",
+    //     field: "title",
+    //   });
+    // }
+
+    // if (
+    //   // !req.body.author ||
+    //   // typeof req.body.author !== "string" ||
+    //   req.body.author.length > 20
+    // ) {
+    //   errorsMessages.push({
+    //     message: "Invalid author",
+    //     field: "author",
+    //   });
+    // }
+
+    // if (errorsMessages.length > 0) {
+    //   return res.status(400).send({ errorsMessages });
+    // }
+
+    if (req.body.title) {
+      video.title = req.body.title.toString();
+    }
+    if (req.body.author) {
+      video.author = req.body.author.toString();
+    }
+
+    if (req.body.canBeDownloaded) {
+      video.canBeDownloaded = req.body.canBeDownloaded.toString();
+    }
+    if (req.body.minAgeRestriction) {
+      video.minAgeRestriction = req.body.minAgeRestriction.toString();
+    }
+
+    if (req.body.availableResolutions) {
+      video.availableResolutions = req.body.availableResolutions.toString();
+    }
+    res.status(200).send(video);
+  }
+});
+
+app.delete("/videos/:id", (req: Request, res: Response) => {
+  for (let i = 0; i < videos.length; i++) {
+    if (videos[i].id === +req.params.id) {
+      videos.splice(i, 1);
+      res.sendStatus(204);
+      return;
+    }
+  }
+  res.send(404);
+});
+
 app.listen(port, () => {
   console.log(`HO1API-V1 app listening on port ${port}`);
 });
