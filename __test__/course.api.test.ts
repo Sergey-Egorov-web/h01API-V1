394bca7a37a5408ee1b3d964f6ac8d5417021f49
Sergey-Egorov-web <ispanezzz.foto@gmail.com>
2024-11-16 17:41:33 +0300

    added post test

--- __test__/course.api.test.ts
+++ __test__/course.api.test.ts
@@ -1,5 +1,6 @@
-import request from "supertest";
+import request, { Response } from "supertest";
 import { app } from "../src/settings";
+import { response } from "express";
 
 describe("/", () => {
   it("should return 200 and empty array", () => {
@@ -13,5 +14,33 @@ describe("/videos", () => {
   });
   it("should return 200 and empty array", async () => {
     await request(app).get("/videos").expect(200, []);
+  });
+
+  it("should create new video with correct input data", async () => {
+    const response: Response = await request(app)
+      .post("/videos")
+      .send({
+        title: "new title",
+        author: "string",
+        minAgeRestriction: "12",
+        availableResolutions: ["P144"],
+      })
+      .expect(201);
+
+    expect(response.body).toEqual({
+      id: expect.any(Number),
+      title: "new title",
+      author: "string",
+      canBeDownloaded: true,
+      minAgeRestriction: "12",
+      // createdAt: expect.any(String),
+      createdAt: expect.stringMatching(
+        /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/
+      ),
+      publicationDate: expect.stringMatching(
+        /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/
+      ),
+      availableResolutions: ["P144"],
+    });
   });
 });
