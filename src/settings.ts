6195b5d10a301dbbd323d3c24bf2a561cbe13d7c
Sergey-Egorov-web <ispanezzz.foto@gmail.com>
2024-11-17 18:22:20 +0300

    bug fixes

--- src/settings.ts
+++ src/settings.ts
@@ -16,16 +16,16 @@ let videos = [
     publicationDate: "2023-10-02T12:00:00Z",
     availableResolutions: ["720p", "1080p"],
   },
-  {
-    id: 2,
-    title: "My video",
-    author: "Ivan",
-    canBeDownloaded: false,
-    minAgeRestriction: null,
-    createdAt: "2024-11-14T21:22:00Z",
-    publicationDate: "2024-11-14T21:25:00Z",
-    availableResolutions: ["720p"],
-  },
+  // {
+  //   id: 2,
+  //   title: "My video",
+  //   author: "Ivan",
+  //   canBeDownloaded: false,
+  //   minAgeRestriction: null,
+  //   createdAt: "2024-11-14T21:22:00Z",
+  //   publicationDate: "2024-11-14T21:25:00Z",
+  //   availableResolutions: ["720p"],
+  // },
 ];
 
 const parserMiddleware = bodyParser({});
