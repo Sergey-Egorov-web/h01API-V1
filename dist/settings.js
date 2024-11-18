19ed7d1f925493463be0ffd05349a856abae022b
Sergey-Egorov-web <ispanezzz.foto@gmail.com>
2024-11-18 06:10:03 +0300

    improve PUT request

--- dist/settings.js
+++ dist/settings.js
@@ -32,16 +32,16 @@ let videos = [
         publicationDate: "2023-10-02T12:00:00Z",
         availableResolutions: ["720p", "1080p"],
     },
-    // {
-    //   id: 2,
-    //   title: "My video",
-    //   author: "Ivan",
-    //   canBeDownloaded: false,
-    //   minAgeRestriction: null,
-    //   createdAt: "2024-11-14T21:22:00Z",
-    //   publicationDate: "2024-11-14T21:25:00Z",
-    //   availableResolutions: ["720p"],
-    // },
+    {
+        id: 2,
+        title: "My video",
+        author: "Ivan",
+        canBeDownloaded: false,
+        minAgeRestriction: null,
+        createdAt: "2024-11-14T21:22:00Z",
+        publicationDate: "2024-11-14T21:25:00Z",
+        availableResolutions: ["720p"],
+    },
 ];
 const parserMiddleware = (0, body_parser_1.default)({});
 exports.app.use(parserMiddleware);
@@ -71,45 +71,18 @@ exports.app.post("/videos", (req, res) => {
     if (!Array.isArray(req.body.availableResolutions) ||
         req.body.availableResolutions.length === 0 ||
         !req.body.availableResolutions.every(isValidResolution)) {
-        // res.status(400).send({
-        //   errorsMessages: [
-        //     {
-        //       message: "availableResolutions field is incorrect",
-        //       field: "availableResolutions",
-        //     },
-        //   ],
-        // });
-        // return;
         errorsMessages.push({
             message: "availableResolutions field is incorrect",
             field: "availableResolutions",
         });
     }
     if (!req.body.author || req.body.author.length > 20) {
-        // res.status(400).send({
-        //   errorsMessages: [
-        //     {
-        //       message: "Author field is incorrect",
-        //       field: "author",
-        //     },
-        //   ],
-        // });
-        // return;
         errorsMessages.push({
             message: "Author field is incorrect",
             field: "author",
         });
     }
     if (!req.body.title || req.body.title.length > 40) {
-        // res.status(400).send({
-        //   errorsMessages: [
-        //     {
-        //       message: "Title field is incorrect",
-        //       field: "title",
-        //     },
-        //   ],
-        // });
-        // return;
         errorsMessages.push({
             message: "Title field is incorrect",
             field: "title",
@@ -139,64 +112,67 @@ exports.app.post("/videos", (req, res) => {
 });
 exports.app.put("/videos/:id", (req, res) => {
     let video = videos.find((p) => p.id === +req.params.id);
-    if (!req.body.title || !req.body.author || !req.body.availableResolutions) {
-        res.status(400).send({
-            errorsMessages: [
-                {
-                    message: "Required fields are missing",
-                    field: "title, author, availableResolutions",
-                },
-            ],
-        });
-        return;
-    }
+    // if (!req.body.title || !req.body.author || !req.body.availableResolutions) {
+    //   res.status(400).send({
+    //     errorsMessages: [
+    //       {
+    //         message: "Required fields are missing",
+    //         field: "title, author, availableResolutions",
+    //       },
+    //     ],
+    //   });
+    //   return;
+    // }
+    const errorsMessages = [];
     if (!video) {
         res.sendStatus(404);
-    }
-    else {
-        // const errorsMessages: { message: string; field: string }[] = [];
-        // if (
-        //   // !req.body.title ||
-        //   // typeof req.body.title !== "string" ||
-        //   req.body.title.length > 40
-        // ) {
-        //   errorsMessages.push({
-        //     message: "Invalid title",
-        //     field: "title",
-        //   });
-        // }
-        // if (
-        //   // !req.body.author ||
-        //   // typeof req.body.author !== "string" ||
-        //   req.body.author.length > 20
-        // ) {
-        //   errorsMessages.push({
-        //     message: "Invalid author",
-        //     field: "author",
-        //   });
-        // }
-        // if (errorsMessages.length > 0) {
-        //   return res.status(400).send({ errorsMessages });
-        // }
-        if (req.body.title) {
-            video.title = req.body.title.toString();
-        }
-        if (req.body.author) {
-            video.author = req.body.author.toString();
-        }
-        if (req.body.canBeDownloaded) {
-            video.canBeDownloaded = req.body.canBeDownloaded.toString();
-        }
-        if (req.body.minAgeRestriction) {
-            video.minAgeRestriction = req.body.minAgeRestriction.toString();
-        }
-        if (req.body.availableResolutions) {
-            video.availableResolutions = req.body.availableResolutions.toString();
-        }
-        res.sendStatus(204);
-        // res.status(204).send(video);
         return;
     }
+    if (!req.body.title ||
+        typeof req.body.title !== "string" ||
+        req.body.title.length > 40) {
+        errorsMessages.push({
+            message: "Invalid title",
+            field: "title",
+        });
+    }
+    if (!req.body.author ||
+        typeof req.body.author !== "string" ||
+        req.body.author.length > 20) {
+        errorsMessages.push({
+            message: "Invalid author",
+            field: "author",
+        });
+    }
+    if (!Array.isArray(req.body.availableResolutions) ||
+        req.body.availableResolutions.length === 0 ||
+        !req.body.availableResolutions.every(isValidResolution)) {
+        errorsMessages.push({
+            message: "availableResolutions field is incorrect",
+            field: "availableResolutions",
+        });
+    }
+    if (errorsMessages.length > 0) {
+        return res.status(400).send({ errorsMessages });
+    }
+    if (req.body.title) {
+        video.title = req.body.title.toString();
+    }
+    if (req.body.author) {
+        video.author = req.body.author.toString();
+    }
+    if (req.body.canBeDownloaded) {
+        video.canBeDownloaded = req.body.canBeDownloaded.toString();
+    }
+    if (req.body.minAgeRestriction) {
+        video.minAgeRestriction = req.body.minAgeRestriction.toString();
+    }
+    if (req.body.availableResolutions) {
+        video.availableResolutions = req.body.availableResolutions.toString();
+    }
+    res.sendStatus(204);
+    // res.status(204).send(video);
+    return;
 });
 exports.app.delete("/videos/:id", (req, res) => {
     for (let i = 0; i < videos.length; i++) {
--- src/settings.ts
+++ src/settings.ts
@@ -30,16 +30,16 @@ let videos = [
     publicationDate: "2023-10-02T12:00:00Z",
     availableResolutions: ["720p", "1080p"],
   },
-  // {
-  //   id: 2,
-  //   title: "My video",
-  //   author: "Ivan",
-  //   canBeDownloaded: false,
-  //   minAgeRestriction: null,
-  //   createdAt: "2024-11-14T21:22:00Z",
-  //   publicationDate: "2024-11-14T21:25:00Z",
-  //   availableResolutions: ["720p"],
-  // },
+  {
+    id: 2,
+    title: "My video",
+    author: "Ivan",
+    canBeDownloaded: false,
+    minAgeRestriction: null,
+    createdAt: "2024-11-14T21:22:00Z",
+    publicationDate: "2024-11-14T21:25:00Z",
+    availableResolutions: ["720p"],
+  },
 ];
 
 const parserMiddleware = bodyParser({});
@@ -74,15 +74,6 @@ app.post("/videos", (req: Request, res: Response) => {
     req.body.availableResolutions.length === 0 ||
     !req.body.availableResolutions.every(isValidResolution)
   ) {
-    // res.status(400).send({
-    //   errorsMessages: [
-    //     {
-    //       message: "availableResolutions field is incorrect",
-    //       field: "availableResolutions",
-    //     },
-    //   ],
-    // });
-    // return;
     errorsMessages.push({
       message: "availableResolutions field is incorrect",
       field: "availableResolutions",
@@ -90,30 +81,12 @@ app.post("/videos", (req: Request, res: Response) => {
   }
 
   if (!req.body.author || req.body.author.length > 20) {
-    // res.status(400).send({
-    //   errorsMessages: [
-    //     {
-    //       message: "Author field is incorrect",
-    //       field: "author",
-    //     },
-    //   ],
-    // });
-    // return;
     errorsMessages.push({
       message: "Author field is incorrect",
       field: "author",
     });
   }
   if (!req.body.title || req.body.title.length > 40) {
-    // res.status(400).send({
-    //   errorsMessages: [
-    //     {
-    //       message: "Title field is incorrect",
-    //       field: "title",
-    //     },
-    //   ],
-    // });
-    // return;
     errorsMessages.push({
       message: "Title field is incorrect",
       field: "title",
@@ -148,70 +121,82 @@ app.post("/videos", (req: Request, res: Response) => {
 app.put("/videos/:id", (req: Request, res: Response) => {
   let video = videos.find((p) => p.id === +req.params.id);
 
-  if (!req.body.title || !req.body.author || !req.body.availableResolutions) {
-    res.status(400).send({
-      errorsMessages: [
-        {
-          message: "Required fields are missing",
-          field: "title, author, availableResolutions",
-        },
-      ],
-    });
-    return;
-  }
+  // if (!req.body.title || !req.body.author || !req.body.availableResolutions) {
+  //   res.status(400).send({
+  //     errorsMessages: [
+  //       {
+  //         message: "Required fields are missing",
+  //         field: "title, author, availableResolutions",
+  //       },
+  //     ],
+  //   });
+  //   return;
+  // }
+
+  const errorsMessages: { message: string; field: string }[] = [];
 
   if (!video) {
     res.sendStatus(404);
-  } else {
-    // const errorsMessages: { message: string; field: string }[] = [];
-
-    // if (
-    //   // !req.body.title ||
-    //   // typeof req.body.title !== "string" ||
-    //   req.body.title.length > 40
-    // ) {
-    //   errorsMessages.push({
-    //     message: "Invalid title",
-    //     field: "title",
-    //   });
-    // }
-
-    // if (
-    //   // !req.body.author ||
-    //   // typeof req.body.author !== "string" ||
-    //   req.body.author.length > 20
-    // ) {
-    //   errorsMessages.push({
-    //     message: "Invalid author",
-    //     field: "author",
-    //   });
-    // }
-
-    // if (errorsMessages.length > 0) {
-    //   return res.status(400).send({ errorsMessages });
-    // }
-
-    if (req.body.title) {
-      video.title = req.body.title.toString();
-    }
-    if (req.body.author) {
-      video.author = req.body.author.toString();
-    }
-
-    if (req.body.canBeDownloaded) {
-      video.canBeDownloaded = req.body.canBeDownloaded.toString();
-    }
-    if (req.body.minAgeRestriction) {
-      video.minAgeRestriction = req.body.minAgeRestriction.toString();
-    }
-
-    if (req.body.availableResolutions) {
-      video.availableResolutions = req.body.availableResolutions.toString();
-    }
-    res.sendStatus(204);
-    // res.status(204).send(video);
     return;
   }
+
+  if (
+    !req.body.title ||
+    typeof req.body.title !== "string" ||
+    req.body.title.length > 40
+  ) {
+    errorsMessages.push({
+      message: "Invalid title",
+      field: "title",
+    });
+  }
+
+  if (
+    !req.body.author ||
+    typeof req.body.author !== "string" ||
+    req.body.author.length > 20
+  ) {
+    errorsMessages.push({
+      message: "Invalid author",
+      field: "author",
+    });
+  }
+
+  if (
+    !Array.isArray(req.body.availableResolutions) ||
+    req.body.availableResolutions.length === 0 ||
+    !req.body.availableResolutions.every(isValidResolution)
+  ) {
+    errorsMessages.push({
+      message: "availableResolutions field is incorrect",
+      field: "availableResolutions",
+    });
+  }
+
+  if (errorsMessages.length > 0) {
+    return res.status(400).send({ errorsMessages });
+  }
+
+  if (req.body.title) {
+    video.title = req.body.title.toString();
+  }
+  if (req.body.author) {
+    video.author = req.body.author.toString();
+  }
+
+  if (req.body.canBeDownloaded) {
+    video.canBeDownloaded = req.body.canBeDownloaded.toString();
+  }
+  if (req.body.minAgeRestriction) {
+    video.minAgeRestriction = req.body.minAgeRestriction.toString();
+  }
+
+  if (req.body.availableResolutions) {
+    video.availableResolutions = req.body.availableResolutions.toString();
+  }
+  res.sendStatus(204);
+  // res.status(204).send(video);
+  return;
 });
 
 app.delete("/videos/:id", (req: Request, res: Response) => {
